ef9ee4b3b25836a7610190c3a100d93349cdb852
john doe <john@doe.com>
2025-06-11 12:35:17 +0200

    remove: bogus levels

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,9 +4,7 @@ import { Level1 } from './level1/level1';
 import { Level2 } from './level2/level2';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'level1'},
-  { path: 'level1', component: Level1},
-  { path: 'level2', component: Level2}
+  { path: '', pathMatch: 'full', component: Level2},
 ];
 
 @NgModule({
